2dd63773db796ba9cac5749e27ffb3d35cb00078
MASINGITA OTTIS MALULEKE <masingitaottismaluleke@gmail.com>
2024-08-22 01:13:42 -0700

    Correction Number 2

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,13 @@
-# puppet manifest
+# Increase the ULIMIT for the Nginx service
 
-exec {'changeUlimit':
-  command => 'sed -i "s/nofile 5/no file 4096/; s/nofile 4/nofile 1024/" /etc/security/limits.conf',
-  path    => ['/usr/bin', '/bin'],
+# Increase the ULIMIT in the default Nginx config
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
