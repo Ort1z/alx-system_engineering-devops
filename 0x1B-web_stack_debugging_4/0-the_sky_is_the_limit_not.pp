c0c330a18e61885f53ce79670c16703574dec01c
MASINGITA OTTIS MALULEKE <masingitaottismaluleke@gmail.com>
2024-08-22 01:05:23 -0700

    Corrections

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,6 @@
-# Web Stack debugging increase limit of open files per user
-exec { 'fix--for-nginx':
-  environment => ['DIR=/etc/default/nginx',
-                  'OLD=ULIMIT="-n 15"',
-                  'NEW=ULIMIT="-n 15000"'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0, 1]
+# puppet manifest
+
+exec {'changeUlimit':
+  command => 'sed -i "s/nofile 5/no file 4096/; s/nofile 4/nofile 1024/" /etc/security/limits.conf',
+  path    => ['/usr/bin', '/bin'],
 }
